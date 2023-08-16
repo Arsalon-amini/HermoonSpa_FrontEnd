67eb573da643a1d9cd3430f0cd0306f80f2bf653
arsalon-amini <arsalonamini2020@gmail.com>
2023-08-16 15:08:46 +0800

    FAQContent refactoring - separation of concerns

--- src/components/FAQAccordion.tsx
+++ src/components/FAQAccordion.tsx
@@ -1,29 +1,19 @@
 import { Container, Paper } from '@mui/material';
 import React from "react";
 import AccordionItem from './AccordionItem';
+import FAQContent from '../utils/FAQContent';
 
 
 type FAQAccordionProps = {
 
 }
 
-  const faqs = [
-    {
-      title: "What is Netflix?",
-      content: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    },
-    {
-      title: "How much does Netflix cost?",
-      content: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from $8.99 to $17.99 a month. No extra costs, no contracts.",
-    },
-  ]
-
 const FAQAccordion: React.FC<FAQAccordionProps> = () => {
     return (
         <>
         <Container>
         <Paper elevation={3} style={{ padding: "20px" }}>
-          {faqs.map((faq) => (
+          {FAQContent.map((faq) => (
             <AccordionItem
               title={faq.title}
               content={faq.content}
---
+++ src/utils/FAQContent.ts
@@ -0,0 +1,11 @@
+const FAQContent = [
+    {
+      title: "What is Netflix?",
+      content: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    },
+    {
+      title: "How much does Netflix cost?",
+      content: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from $8.99 to $17.99 a month. No extra costs, no contracts.",
+    },
+  ]
+  export default FAQContent;
